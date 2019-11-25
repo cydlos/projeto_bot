25c19cdd20344d59c9012356cff75b5de7f6fe11
cydlos <cydnel@gmail.com>
2019-11-24 21:56:47 -0300

    Configuração do Travis. close #1

---
+++ bot/bot_pack.py
@@ -0,0 +1,18 @@
+class Bot_pack:
+    def bot(self):
+        lista = []
+        arquivo = "bot.txt"
+        with open(arquivo) as obj:
+            conteudos = obj.readlines()
+        for x in conteudos:
+            lista.append(x)
+        return lista
+
+
+if __name__ == "__main__":
+    # Não existe a função __init__, é preciso inicializar com Bot_pack()
+    testando = Bot_pack().bot()
+    if "inter" in testando:
+        print("oi")
+    else:
+        print(testando)
